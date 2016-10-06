b8ed7337a6b857b3f0873b4e12504907dc8c0b45
AndrewSC208 <meilingandrew@gmail.com>
2016-10-06 14:39:22 -0700

    A few updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+
 })
 export class AboutComponent implements OnInit {
+	title: string = "center text"
+	//title: string = "Andrew Meiling | Front-End Developer"
 
   constructor() { }
 
