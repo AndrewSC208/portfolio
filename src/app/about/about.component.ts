f9f73c32f471261fbc2aa87b60230ac10a5c212b
Andrew Meiling <Andrew Meiling>
2016-10-16 21:45:15 -0700

    About is angularifyed

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 	title: string = "center text"
+
 	//title: string = "Andrew Meiling | Front-End Developer"
 
   constructor() { }
@@ -16,14 +17,17 @@ export class AboutComponent implements OnInit {
   }
 
 	cards = [
-		{ title : 'About'},
-		{ title : 'Work'},
-		{ title : 'Education'},
-		{ title : 'Play'},
-	]
+		{ title : 'About Me', text: 'About text! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque nemo animi commodi ad possimus explicabo dolore suscipit. Modi sapiente reiciendis at. Recusandae incidunt dicta maiores autem voluptates blanditiis esse quae.'},
+		{ title : 'Work', text: 'this is some text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus eos nobis inventore illo adipisci officiis debitis ut, molestiae minima dignissimos beatae veritatis non dolore nostrum. Laboriosam doloribus, fugiat fugit tempora!'},
+		{ title : 'Education', text: 'sure another round of text, why not, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste natus cumque, recusandae cupiditate. Odio maiores voluptatibus labore nostrum, dolorum aliquid quisquam, quo aperiam, dicta hic quam iusto. Tenetur, atque, quidem.'},
+		{ title : 'Play', text: 'And once again a different round of text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore impedit, sapiente blanditiis vel, quaerat cumque! Vel odio corrupti autem quos. Nisi ex similique incidunt excepturi quam commodi culpa iure corporis.'},
+	];
+
+	// need to make a CARD interface
+	selectedCard = this.cards[0];
 
 	onSelect(card) {
-		console.log(card);
+		this.selectedCard = card;
 	}
 
 }
