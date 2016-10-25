2063211d3d20767af3d998f71ef82a3ee2610513
Andrew Meiling <Andrew Meiling>
2016-10-24 17:09:55 -0700

    added nav

--- src/app/shared/main-nav/main-nav.component.ts
+++ src/app/shared/main-nav/main-nav.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent implements OnInit {
+	isClassVisible: false;
 
   constructor() { }
 
