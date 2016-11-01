7c6fe281e11c13358cbe92bdcfeb52e41a0eb1fb
Andrew Meiling <Andrew Meiling>
2016-11-01 09:09:43 -0700

    Added content

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -17,10 +17,9 @@ export class AboutComponent implements OnInit {
   }
 
 	cards = [
-		{ title : 'About Me', text: 'About!!'},
-		{ title : 'Work', text: 'this is some text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus eos nobis inventore illo adipisci officiis debitis ut, molestiae minima dignissimos beatae veritatis non dolore nostrum. Laboriosam doloribus, fugiat fugit tempora!'},
-		{ title : 'Education', text: 'sure another round of text, why not, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste natus cumque, recusandae cupiditate. Odio maiores voluptatibus labore nostrum, dolorum aliquid quisquam, quo aperiam, dicta hic quam iusto. Tenetur, atque, quidem.'},
-		{ title : 'Play', text: 'And once again a different round of text, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore impedit, sapiente blanditiis vel, quaerat cumque! Vel odio corrupti autem quos. Nisi ex similique incidunt excepturi quam commodi culpa iure corporis.'},
+		{ title : "About Me", text: "Front-End Developer with a background in marketing and management. </br> I’m currently working as a Web Developer and Online Project Manager for Luth Research, an independent market research firm that conducts online surveys for large corporations. At Luth, I primarily work in Python, JavaScript, HTML, and CSS to code online surveys. These surveys are delivered to millions of people through the company’s proprietary mobile app SavvyConnect, and the data is used to fuel business intelligence. </br> In addition to my work at Luth, I also do part-time Front-End work for a local software startup in San Diego called Urban Translations. </br> I have a particular interest in working in Angular 2 and all things JavaScript, building applications with attractive and practical user interfaces."},
+		{ title : 'Education', text: "Evangel University, Bachelor of Science in Marketing, 2009 </br> Evangel University, Bachelor of Science in Business Management </br> CONTINUED EDUCATION </br> Current course:​ MongoDB University, NodeSchool.io (“Learn You Node”) – to gain a better understanding of asynchronous JavaScript, and JavaScript server technologies Completed course:​ Java 1 (Rio Salado College), Codecademy.com (HTML, CSS, JavaScript, Angular.js)"},
+		{ title : 'Play', text: "When I'm not working, I spend my free time surfing, gaming, listening to podcasts, cooking, and hiking with my wife Brittany and Labrador pup Indie."},
 	];
 
 	// need to make a CARD interface
