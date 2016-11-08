eda244055e205b6d040d09e9ef0515ce63f180a4
AndrewSC208 <meilingandrew@gmail.com>
2016-11-07 21:26:09 -0800

    a few touch ups

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -18,8 +18,7 @@ export class AboutComponent implements OnInit {
 
 	cards = [
 		{ title : "About Me", text: 
-			[ "I’m currently working as a Web Developer and Online Project Manager for Luth Research, an independent market research firm that conducts online surveys for large corporations. At Luth, I primarily work in Python, JavaScript, HTML, and CSS to code online surveys.", 
-			 "In addition to my work at Luth, I also do part-time Front-End work for a local software startup in San Diego called Urban Translations.", 
+			[ "I’m currently working as a Web Developer and Online Project Manager for Luth Research, an independent market research firm that conducts online surveys for large corporations. At Luth, I primarily work in Python, JavaScript, HTML, and CSS to build solutions for online market research.",
 			 "I have a particular interest in working in Angular 2 and all things JavaScript, building applications with attractive and practical user interfaces."
 			]},
 		{ title : 'Education', text: 
@@ -29,7 +28,7 @@ export class AboutComponent implements OnInit {
 			 "Java 1 (Rio Salado College), Codecademy.com (HTML, CSS, JavaScript, Angular.js), 'Learn Python The Hard Way' by Zed A. Shaw, 'JavaScript The Definitive Guild' by David Flanagan, MongoDB University, NodeSchool.io (“Learn You Node”), and avid Pluralsight user"
 			]},
 		{ title : 'Play', text: 
-			["When I'm not working, I spend my free time surfing, gaming, listening to podcasts, cooking, and hiking with my wife Brittany and Labrador pup Indie."]},
+			["When I'm building the internet, I spend my free time surfing, gaming, listening to podcasts, cooking, and hiking with my wife Brittany and Labrador pup Indie."]},
 	];
 
 	// need to make a CARD interface
